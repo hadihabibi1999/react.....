377fda074636e85ad91181bc817b3f7a77ed6847
HadiCyb <hadihabibi2000@gmail.com>
2020-04-30 19:48:26 +0430

    componentdidMount

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  tick() {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  }
+
+  componentDidMount(){
+    setInterval(()=>{this.tick()},1000)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>{this.state.count} secounds</div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
